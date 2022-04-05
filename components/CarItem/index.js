ab28816f63cc468e9950987caf88756e9bbf9411
Alen <a.eftimov@site-works.de>
2022-04-05 12:50:24 +0200

    Car Item Component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -4,35 +4,46 @@ import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const CarItem = (props) => {
+
+    const { name, tagline, taglineCTA, image } = props;
     
     return (
         <View style={styles.carContainer}>
 
             <ImageBackground 
-            source={require('../../assets/images/tX.jpeg')} 
+            // source={require('../../assets/images/tX.jpeg')} 
+            source={image} 
             style={styles.image}
             />
 
             <View style={styles.titles}>
-                <Text style={styles.title}>Model S</Text>
-                <Text style={styles.subtitle}>Starting at $73,560</Text>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.subtitle}>
+                    {tagline}
+                    {' '}
+                    <Text style={styles.taglineCTA}>
+                        {taglineCTA}
+                    </Text>
+                </Text>
             </View>
 
-            <StyledButton
-             type="primary"
-             content={"Custom Order"} 
-             onPress={() => {
-                 console.warn("Custom Order was pressed");
-             }} 
-            />
+            <View style={styles.buttonsContainer}>
+                <StyledButton
+                    type="primary"
+                    content={"Custom Order"} 
+                    onPress={() => {
+                        console.warn("Custom Order was pressed");
+                    }} 
+                />
 
-            <StyledButton
-             type="secondary"
-             content={"Existing Inventory"} 
-             onPress={() => {
-                 console.warn("Existing Inventory was pressed");
-             }} 
-            />
+                <StyledButton
+                    type="secondary"
+                    content={"Existing Inventory"} 
+                    onPress={() => {
+                        console.warn("Existing Inventory was pressed");
+                    }} 
+                />
+            </View>            
 
         </View>
     );
